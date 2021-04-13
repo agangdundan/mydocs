61c624fb1ae2d246b50726645dd2d1371f6d7f83
agangdundan <agangdundan@users.noreply.github.com>
2021-04-12 19:34:08 -0500

    Update deploy.sh
    更新部署文件24

--- deploy.sh
+++ deploy.sh
@@ -36,6 +36,7 @@ git push -f https://${access_token}@github.com/agangdundan/mydocs.git
 cd /home/travis/build/agangdundan/mydocs/docs/.vuepress/dist-gitee
 git init
 git add -A
+git commit -m 'deploy'
 git remote add gitee-mydocs https://gitee.com/agangdundan/mydocs.git
 git branch -M gh-pages
 # 推送至gitee
